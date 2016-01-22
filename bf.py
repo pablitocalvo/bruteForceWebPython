fb6f358efc9bf3990f418b06307dc7fccd6f2034
pablitocalvo <sbtpla68@yahoo.it>
2016-01-22 17:46:43 +0100

    aggiunto qualche commento a bf.py

--- bf.py
+++ bf.py
@@ -1,14 +1,16 @@
 import urllib
 import urllib2
 
+#TODO : ottimizzare con controlli ecc.
+
 url = 'http://pablitocalvo.altervista.org/bucaquestosito/controlloPassword.php'
 values = {'utente' : 'paolo',
           'password' : 'chiave',
-           }
+           }					# prepara un oggetto in notazione JSON
 
-data = urllib.urlencode(values)
-req = urllib2.Request(url, data)
-response = urllib2.urlopen(req)
-the_page = response.read()
+data = urllib.urlencode(values)   #"Convert a mapping object or a sequence of two-element tuples to a “percent-encoded” string, suitable to pass to urlopen()"
+req = urllib2.Request(url, data)  # prepara la richiesta 	
+response = urllib2.urlopen(req)   # apre la connessione richiesta 
+the_page = response.read()        
 print the_page
 
